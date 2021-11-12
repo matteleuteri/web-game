c88a69869a72390b662a4915265eba19a631dd99
Matthew Eleuteri <meleuteri2@gmail.com>
2021-11-12 18:49:34 -0500

    fixed character speed

--- game.js
+++ game.js
@@ -4,36 +4,35 @@ function drawCanvas() {
     ctx.clearRect(0, 0, c.width, c.height);
     ctx.beginPath();
     ctx.moveTo(xPos, yPos);
-    ctx.lineTo(xPos + 20, yPos + 40);
-    ctx.lineTo(xPos - 20, yPos + 40);
+    ctx.lineTo(xPos + character_width, yPos);
+    ctx.lineTo(xPos + character_width, yPos + character_height);
+    ctx.lineTo(xPos, yPos + character_height)
     ctx.fill();
 	ctx.stroke();
 }
-
 
 $(document).keydown(function(e) {
     if (e.keyCode > 36 && e.keyCode < 41)
 	    direction = e.keyCode - 37;
 });
 
-
 function update(progress, dir) {
     // Update the state of the world for the elapsed time since last render
 	if (dir == 0) {
-		xPos -= progress;
-		if (xPos < 0) {xPos = 590;}
+		xPos -= (progress * speed);
+		if (xPos < -1 * character_width) {xPos = c_width;}
 	}
 	else if (dir == 1) {
-		yPos -= progress;
-		if (yPos < 0) {yPos = 390;}
+		yPos -= (progress * speed);
+		if (yPos < -1 * character_height) {yPos = c_height;}
 	}
 	else if (dir == 2) {
-		xPos += progress;
-		if (xPos > 600) {xPos = 10;}
+		xPos += (progress * speed);
+		if (xPos > c_width ) {xPos = -1 * character_width;}
 	}
 	else if (dir == 3) {
-		yPos += progress;
-		if (yPos > 400) {yPos = 10;}
+		yPos += (progress * speed);
+		if (yPos > c_height) {yPos = -1 * character_height;}
 	}
 }
 
@@ -47,15 +46,14 @@ function loop(timestamp) {
   window.requestAnimationFrame(loop);
 }
 
-xPos = 0;
-yPos = 0;
-incr = 1;
-c_width = 600;
-c_height = 400;
-character_width = 20;
-character_height = 20;
-
-direction = 0;
+var xPos = 100;
+var yPos = 100;
+var speed = 0.2;
+var c_width = 600;
+var c_height = 400;
+var character_width = 20;
+var character_height = 20;
+var direction = 0;
 var lastRender = 0;
 
 window.requestAnimationFrame(loop);
