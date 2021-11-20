df6b17b2a6d25f1a6f171db9079aff8c9a42b245
Matthew Eleuteri <meleuteri2@gmail.com>
2021-11-19 20:44:07 -0500

    .

--- server/server.js
+++ server/server.js
@@ -22,23 +22,20 @@ io.on('connection', (socket) => {
   let client_id = socket.id;
   console.log('(s)A user just connected with id ' + client_id + '.');
  	players[client_id] = {'xPos': 100, 'yPos': 100, 'speed': 0.2, 'direction': 0};
- 	
   socket.emit('createPlayerProfile', client_id);
  	num_players += 1;
   socket.on('updateConfig', (id_and_configs) => {
-    //console.log(id_and_configs);
     let c_id = id_and_configs['id'];
     let c_config = id_and_configs['configs']
-
-    if(c_id == -1){return;}
+    if(c_id == -1) return;
  		players[c_id]['xPos'] = c_config['xPos'];
  		players[c_id]['yPos'] = c_config['yPos'];
  		players[c_id]['speed'] = c_config['speed'];
  		players[c_id]['direction'] = c_config['direction'];
  	});
   socket.on('disconnect', () => {
-    console.log('A user has disconnected with id ' + socket.id);
-    delete players[socket.id];
+    console.log('A user has disconnected with id ' + client_id);
+    delete players[client_id];
   });
 });
 
